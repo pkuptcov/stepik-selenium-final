da5e7c918e90c398f1d3c7e7da2b80b2ee4796be
pkuptcov <pavel.kuptcov@gmail.com>
2019-12-07 22:33:45 +0300

    add is_element_present

--- pages/base_page.py
+++ pages/base_page.py
@@ -10,3 +10,9 @@ class BasePage():
     def open(self):
         self.browser.get(self.url)
 
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
