8eb6c9717f06aa6dfa4402188dad671c3181c8ee
pkuptcov <pavel.kuptcov@gmail.com>
2019-12-07 23:30:58 +0300

    create test_should_be_login_page

---
+++ pages/test_login_page.py
@@ -0,0 +1,8 @@
+from pages.login_page import LoginPage
+
+
+def test_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
