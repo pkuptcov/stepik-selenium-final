db959f0ccf2f90ee0d0a07858a37d9c7502a3612
pkuptcov <pavel.kuptcov@gmail.com>
2019-12-08 16:30:25 +0300

    add new locators in class ProductPageLocator

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+
+class ProductPageLocator:
+    ADD_TO_BASKET_BUTTON = (By.XPATH, "//button[@type='submit'][@class='btn btn-lg btn-primary btn-add-to-basket']")
+    SUCCESS_MESSAGE = (By.XPATH, "(//div[@class='alertinner '])[1]")
+    PRODUCT_TITLE = (By.XPATH, "//h1")
+    PRODUCT_PRICE = (By.XPATH, "//p[@class='price_color']")
+    BASKET_TOTAL = (By.XPATH, "//div[@class='basket-mini pull-right hidden-xs']")
