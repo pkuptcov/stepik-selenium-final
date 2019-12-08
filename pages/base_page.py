1b70cf50729b55d5bfcaa1605c6d0fef04cef316
pkuptcov <pavel.kuptcov@gmail.com>
2019-12-08 22:35:22 +0300

    add methods go_to_login_page should_be_login_link

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,8 @@ import math
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from pages.locators import BasePageLocators
+
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -50,3 +52,10 @@ class BasePage():
             return False
 
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
