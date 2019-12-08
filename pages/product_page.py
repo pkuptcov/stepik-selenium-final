e6fd2a4177213a20ae62f6684d0872d641135118
pkuptcov <pavel.kuptcov@gmail.com>
2019-12-08 16:31:06 +0300

    add ProductPage and methods

---
+++ pages/product_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+from .locators import ProductPageLocator
+
+
+class ProductPage(BasePage):
+    def add_to_cart(self):
+        add_to_cart_button = self.browser.find_element(*ProductPageLocator.ADD_TO_BASKET_BUTTON)
+        add_to_cart_button.click()
+
+    def should_be_right_title(self):
+        product_title = self.browser.find_element(*ProductPageLocator.PRODUCT_TITLE).text
+        success_message = self.browser.find_element(*ProductPageLocator.SUCCESS_MESSAGE).text
+        assert product_title in success_message, "Product title doesn't match product name added"
+
+    def should_be_right_price(self):
+        product_price = self.browser.find_element(*ProductPageLocator.PRODUCT_PRICE).text
+        basket_total = self.browser.find_element(*ProductPageLocator.BASKET_TOTAL).text
+        assert product_price in basket_total, "Product price isn't equal to basket price"
