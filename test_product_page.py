69c297aac61f29f87bc0587353bd0737ffd8610b
pkuptcov <pavel.kuptcov@gmail.com>
2019-12-09 21:21:09 +0300

    add class TestUserAddToBasketFromProductPage with setup and two tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
 from pages.product_page import ProductPage
 import pytest
 from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
+import time
 
 
 @pytest.mark.skip
@@ -70,3 +72,28 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket_page()
     page.should_not_be_products_in_basket()
     page.should_be_message_empty_basket()
+
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user(email=str(time.time())+"@fakemail.org", password='Qw11111111!')
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_cart()
+        page.solve_quiz_and_get_code()
+        page.should_be_right_title()
+        page.should_be_right_price()
